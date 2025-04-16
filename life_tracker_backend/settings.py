bf4fe5bbabb1def02fbb9daf2b3bd0611766bb28
Ayat166 <ayatali1661@gmail.com>
2025-04-16 21:38:59 +0200

    added formatter

--- life_tracker_backend/settings.py
+++ life_tracker_backend/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
-    'corsheaders',
-    'tracker',
+    "rest_framework",
+    "corsheaders",
+    "tracker",
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
