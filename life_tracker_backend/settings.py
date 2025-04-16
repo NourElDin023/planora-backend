131d9f3ce387577f708073bdcea14cef63ef62fc
NourElDin Nabil <nournabil1012@gmail.com>
2025-04-16 22:55:24 +0200

    replace hardcoded SECRET_KEY with config variable

--- life_tracker_backend/settings.py
+++ life_tracker_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h0ay=_u9l+wi^8r^^_b!)1^*vdmsr6=#(h$o*23n&5yp-8j*9t"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
