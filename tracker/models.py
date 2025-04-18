a1e50359848fe6ccd3d0ad7f4c3732f95a003c4b
the-aphrodite <reem9544@yahoo.com>
2025-04-19 00:26:26 +0200

    added dark mode

--- tracker/models.py
+++ tracker/models.py
@@ -1,6 +1,14 @@
+from django.conf import settings
 from django.db import models
 
 class Note(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notes',
+        null=True,        # allow null values in the DB
+        blank=True        # allow it to be optional in forms
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- tracker/views.py
+++ tracker/views.py
@@ -8,35 +8,40 @@ from django.http import JsonResponse
 @api_view(['GET', 'POST'])
 def notes_list(request):
     if request.method == 'GET':
-        notes = Note.objects.all()
+        notes = Note.objects.filter(user=request.user)  
         serializer = NoteSerializer(notes, many=True)
         return Response(serializer.data)
+    
     elif request.method == 'POST':
         serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)  
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def note_detail(request, pk):
     try:
-        note = Note.objects.get(pk=pk)
+        note = Note.objects.get(pk=pk, user=request.user)  
     except Note.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = NoteSerializer(note)
         return Response(serializer.data)
+    
     elif request.method == 'PUT':
         serializer = NoteSerializer(note, data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     elif request.method == 'DELETE':
         note.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
     
 def hello(request):
     return JsonResponse({'message': 'Welcome to the Notes App!'})
