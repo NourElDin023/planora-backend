b778b349eb0d5bded24619ef57e4314611d081dc
NourElDin Nabil <nournabil1012@gmail.com>
2025-04-19 14:27:39 +0200

    Add API test page and update URL routing
    - Created a new HTML page for API testing (login_test.html).
    - Updated settings to include the new templates directory.
    - Added a new URL pattern for the API test page in urls.py.
    - Implemented a view function to render the API test page.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserProfileView, UserListView
+from .views import RegisterView, LoginView, UserProfileView, UserListView, api_test_view
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path("profile/", UserProfileView.as_view(), name="profile"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("all-usernames/", UserListView.as_view(), name="all-usernames"),
+    # New URL pattern for the API testing page
+    path("test-api/", api_test_view, name="test-api"),
 ]
