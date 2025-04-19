426712a1bed7771eaf23b8b4da719c0715c75120
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2025-04-19 18:05:09 +0200

    add routes tasks app

--- life_tracker_backend/urls.py
+++ life_tracker_backend/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("api/users/", include("users.urls")),
     path("api/pages/", include("pages.urls")),
     path("api/notifications/", include("notifications.urls")),
+    path("api/tasks/", include("tasks.urls")),
 ]
