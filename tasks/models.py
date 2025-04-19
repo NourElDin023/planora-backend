10efd2bfde26f2fb2277f6163efdc30c0b1f4b1d
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2025-04-19 18:01:34 +0200

    Add task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="tasks"
+    )
+    title = models.CharField(max_length=255)
+    details = models.TextField(blank=True, null=True)
+    due_date = models.DateField()
+    due_time = models.TimeField()
+    category = models.CharField(max_length=100)
+    task_icon = models.ImageField(upload_to="task_icons/", blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
