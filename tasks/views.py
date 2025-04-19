c0e8d464615af01a689066d97b53d351860ba6d6
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2025-04-19 18:05:52 +0200

    add task view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing task instances.
+    """
+
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Filter tasks by logged in user
+        """
+        return Task.objects.filter(owner=self.request.user).order_by("-due_date", "due_time")
+
+    def perform_create(self, serializer):
+        """
+        Set the owner to the logged in user when creating a task
+        """
+        serializer.save(owner=self.request.user)
