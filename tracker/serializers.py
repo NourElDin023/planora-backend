fd6a141b5622327d15883a031a5ad164caf1c7f8
Ayat166 <ayatali1661@gmail.com>
2025-04-22 21:43:07 +0200

    retrive username in notes

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -3,7 +3,12 @@ from .models import Note
 
 
 class NoteSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Note
-        fields = ["id", "title", "content", "created_at", "updated_at", "task"]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ["id", "title", "content", "created_at", "updated_at", "task", "user"]
+        read_only_fields = ["id", "created_at", "updated_at", "user"]
+
+    def get_user(self, obj):
+        return obj.user.username
