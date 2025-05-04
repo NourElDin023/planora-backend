b4809c50a071db7741871852ad93124e261e7bda
NourEldin Nabil <nournabil1012@gmail.com>
2025-05-04 18:00:43 +0300

    Update swagger URL to be home

--- life_tracker_backend/swagger.py
+++ life_tracker_backend/swagger.py
@@ -31,7 +31,7 @@ Most endpoints require authentication except for registration and login.
 )
 
 swagger_urlpatterns = [
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
 ]
